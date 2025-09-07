f007a705ed9523c31bc2471dab4f3c51fb4da55f
Matthew O'Riordan <matthew.oriordan@gmail.com>
2025-09-07 08:56:59 +0200

    Add sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://websocket.org',
   integrations: [
+    sitemap(),
     starlight({
       title: 'WebSocket.org',
       social: {
